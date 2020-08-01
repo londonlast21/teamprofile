87bdda46286abc061d984d2525c0cb1509173f2f
jack <biffisbuffm@gmail.com>
2020-08-01 12:27:50 -0500

    tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,15 +1,23 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates a employee object', () => {
-    const employee = new Employee('jack');
-
-    expect(employee.name).toBe('jack');
-    expect(employee.name.length).toBeGreaterThan(0);
+    const employee = new Employee();
+    
+    expect(typeof(employee)).toBe("object");
 
 })
 
+test('gets user input for name', () => {
+    const name = "jack";
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+})
+
+
+
 test('gets input for id', () => {
-    const employee = new Employee('jack');
+    const testId = 12345;
+    const employee = new Employee("Id");
     expect(employee.getId()).toEqual(expect.any(Number));
 })
 
