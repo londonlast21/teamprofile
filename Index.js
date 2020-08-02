4f9f3a1a31232677a98077d4eb50b99e71b66cd4
jack <biffisbuffm@gmail.com>
2020-08-01 20:49:28 -0500

    first 3 questions work

--- Index.js
+++ Index.js
@@ -1,55 +1,45 @@
+// NPM modules
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// constructors
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 
-async function Profile(){
 
-    // ask for the user to input information
-    inquirer
-        response = await inquirer.prompt([
-            {
-            type: 'input',
-            name: 'name',
-            message: "What's employee's name?",
-            },
-            {
-            type: 'input',
-            name: 'id',
-            message: "Enter employee's Id number",
-            },
-            {
-            type: 'input',
-            name: 'email',
-            message: "Enter employee's email",
-            },
-            {
-            type: 'list',
-            name: role,
-            message: "Choose employee's role",
-            choices: ['Manager', 'Engineer', 'Intern']
-            },
+// global variables
+const teamMembers = [];
+
+// prompt user for initial input
+inquirer.prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: "What's employee's name?",
+        },
+        {
+        type: 'input',
+        name: 'id',
+        message: "Enter employee's Id number",
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: "Enter employee's email",
+        }
+
         ])
         // do what with the answers?
-        .then(answers => {
-            this.employee = new Employee(name);
-            this.employee = new Employee(id);
-            this.employee = new Employee(email);
-            this.employee = new Employee(role);
-
-            this.startNewProfile();
+        .then(function(response){
+            // send this information to the card constructor
+         
+            console.log(response);
+            
         });
 
+    // make next function for role run
 
-        startNewProfile(){
-            console.log('Your information is:');
-            console.log(this.employee.name);
-            console.log(this.employee.id);
-            console.log(this.employee.email);
-            console.log(this.employee.role);
-        }
-};
 
 
 // // part that creates the HTML page
